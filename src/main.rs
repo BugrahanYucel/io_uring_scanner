a2a65fce5f568f7ff5436f44d7d396923105069e
BugrahanYucel <Bugrahan57@hotmail.com>
2023-07-30 21:32:19 +0300

    Removed unnecessary parts. Progra is able to "TCP connect" to a single ip-port pair

--- src/main.rs
+++ src/main.rs
@@ -12,59 +12,50 @@ use libc::{self, iovec};
 
 use pnet::packet::{Packet, icmp};
 
-struct Ring<'a> {
-    sq: &'a io_uring::SubmissionQueue<'a>,
-    cq: &'a io_uring::CompletionQueue<'a>,
+struct Ring {
+    entries : u32,
+    rx_size : u32,
 }
 
-impl<'a> Ring<'a> {
-    pub fn new (entries: u32) -> Result<Self, Error> {
-        let mut ring = IoUring::new(entries).expect("Error when creating io_uring");
-        let sq = ring.submission();
-        let cq = ring.completion();
-
-        Ok(Self {sq : &sq, cq: &cq})
+impl Ring {
+    // pub fn new (ring : IoUring) -> Result<Self, Error> {
+        // let mut ring = IoUring::new(8).expect("Error when creating io_uring");
         
-    }
+        // let sq: io_uring::SubmissionQueue = ring.submission();
+        // let cq: io_uring::CompletionQueue = ring.completion();
 
-//     pub fn submission (&self) -> Option<&io_uring::SubmissionQueue<'a>> {
-//     }
+        // Ok(Self {})
+    // }
 
-//     pub fn completion (&self) -> Option<&io_uring::CompletionQueue<'a>> {
-//     }
 
-    pub fn submit_sq (&mut self, entry : squeue::Entry) -> Result<(), Error> {
-        unsafe {
-            self.sq.push(&entry).expect("Submission queue is full");
-        }
-        Ok(())
-    }
+    // pub fn submit_sq (&mut self, entry : squeue::Entry) -> Result<(), Error> {
+    //     unsafe {
+    //         self.sq.push(&entry).expect("Submission queue is full");
+    //     }
+    //     Ok(())
+    // }
 
-    pub fn submit_and_wait(&self, num : i32) {
-        // ring.submit_and_wait(1).expect("Completion queue is empty");
+    // pub fn submit_and_wait(&self, num : i32) {
+    //     // ring.submit_and_wait(1).expect("Completion queue is empty");
         
-    }
+    // }
 
-    pub fn return_cqe (&mut self, entry : squeue::Entry) -> Result<(), Error>{
+    // pub fn return_cqe (&mut self, entry : squeue::Entry) -> Result<(), Error>{
 
-        let cqe = self.cq.next().expect("Completion queue is empty");
-        let connect_result = cqe.result();
-        if connect_result < 0 {
-            panic!("Completion queue failed to retrieve cqe")
-        }
+    //     let cqe = self.cq.next().expect("Completion queue is empty");
+    //     let connect_result = cqe.result();
+    //     if connect_result < 0 {
+    //         panic!("Completion queue failed to retrieve cqe")
+    //     }
 
-        let result = cqe.result();
+    //     let result = cqe.result();
 
-        Ok(())
-    }
-
-    // TODO: Do I need other methods for buffered I/O operations, 
-    // or can I implement them on top of these?
-    // I DON'T!
+    //     Ok(())
+    // }
 
 } 
 
-fn tcp_connect (ring : Ring, ip_port : &str) -> Result<i32, io::Error>
+fn scan (ring : IoUring, ip_port : &str) -> Result<i32, io::Error>
 {
     println!("Trying to connect to: {}", ip_port);
 
@@ -80,7 +71,7 @@ fn tcp_connect (ring : Ring, ip_port : &str) -> Result<i32, io::Error>
     for i in 0..4 {
         ip_bytes[i] = ip_itr[i].parse::<u8>().unwrap();
     }
-    println!("{:?}", ip_bytes);
+    println!("Trying: {:?}", ip_bytes);
 
     let addr = SockaddrIn::new(
         ip_bytes[0],
@@ -94,18 +85,30 @@ fn tcp_connect (ring : Ring, ip_port : &str) -> Result<i32, io::Error>
     let sckt = socket(
         AddressFamily::Inet,
         SockType::Stream,
-        SockFlag::empty(), 
+        SockFlag::SOCK_NONBLOCK, 
         None,
     ).expect("TCP socket creation failed");
 
     // Set the socket to non-blocking mode
     // TODO: Chek if as_raw_fd() mmethod is needed or not
-    nix::fcntl::fcntl(sckt, nix::fcntl::FcntlArg::F_SETFL(nix::fcntl::OFlag::O_NONBLOCK)).expect("Failed to set socket to non-blocking");
-    
+    // nix::fcntl::fcntl(sckt, nix::fcntl::FcntlArg::F_SETFL(nix::fcntl::OFlag::O_NONBLOCK)).expect("Failed to set socket to non-blocking");
 
-    /*
-     * Connection
-     */
+    // TODO: Do this with a range of ips and ports
+    connect(sckt, addr, ring).expect("Error when trying to connect");
+
+    Ok(sckt)
+
+}
+
+/*
+* Connection
+*/
+fn connect (
+    sckt : i32, 
+    addr : SockaddrIn,
+    mut ring : IoUring
+    ) -> Result<i32, Error> {
+
 
     // Register opcode to establish connection
     let op_connect: squeue::Entry = opcode::Connect::new(
@@ -116,24 +119,28 @@ fn tcp_connect (ring : Ring, ip_port : &str) -> Result<i32, io::Error>
     .build();
     // .flags(squeue::Flags::IO_LINK);
 
+    // TODO: include timeout
     // let connect_timeout = Timespec::new().sec(5);
     // let op_connect_timeout = opcode::LinkTimeout::new(&connect_timeout).build();
     // let ops = [op_connect, op_connect_timeout];
     
-    let mut tx_buffer: Vec<u8> = vec![0; 128];
-    let fixed_buffer = iovec {
-        iov_base: tx_buffer.as_mut_ptr() as *mut libc::c_void, 
-        iov_len: tx_buffer.len(),
-    };
+    unsafe {
+        ring.submission()
+        .push(&op_connect)
+        .expect("Failed to push connect to submission queue");
+    }
+    let _ = ring.submit();
 
-    let op_recv = opcode::ReadFixed::new(
-        Fd(sckt.as_raw_fd()),
-        fixed_buffer.iov_base.cast::<u8>(),
-        fixed_buffer.iov_len as u32,
-        0,
-    ).build();
+    // TODO: Get connection result
+    let cqe = ring.completion().next().expect("Completion queue is empty");
 
-    Ok(sckt)
+    if cqe.result() >= 0 {
+        println!("Connection established!");
+    } else {
+        println!("Connection failed. Error code: {}", cqe.result());
+    }
+
+    Ok(cqe.result())
 
 }
 
@@ -182,12 +189,11 @@ fn send_tcp_packet (
 }
 
 
-
 fn read_response (
     ring: &mut IoUring,
     sckt: i32,
     buffer: &mut [u8],
-) -> Result<u32, Error> {
+) {
     // let mut rx_buffer: Vec<u8> = vec![0; 1024];
 
     // let rcv_buffer = iovec {
@@ -203,63 +209,23 @@ fn read_response (
 
     unsafe {
         ring.submission().push(&read_e).expect("Submission queue is full");
-    }
-
-    Ok(())
+    };
 }
-
-
-/*
-fn craft_icmp (buf : &mut [u8], sequence_number : u16, identifier : u16) -> MutableEchoRequestPacket<>
-{
-    let mut echo_packet = MutableEchoRequestPacket::new(buf).unwrap();
-    echo_packet.set_sequence_number(sequence_number);
-    echo_packet.set_identifier(identifier);
-    echo_packet.set_icmp_type(IcmpTypes::EchoRequest);
-    echo_packet.set_icmp_code(IcmpCode::new(12));
-
-    let echo_checksum = checksum(&IcmpPacket::new(echo_packet.packet()).unwrap());
-    echo_packet.set_checksum(echo_checksum);
-
-    println!("echo_packet: {:?}", echo_packet);
-
-    return echo_packet;
-    
-    // TODO: Send the packet normally, then test it with io_uring
-}
-*/
-
-/*
-fn send_icmp () {
-
-    let packet_size = EchoRequestPacket::minimum_packet_size();
-    let mut buf = vec![0; packet_size];
-
-    let icmp_packet = craft_icmp(&mut buf[..], 0, 1);
-    let test : &mut str = icmp_packet;
-
-    println!("payload: {:?}", test);
-}
-*/
 
 fn main() {
     // Take arguments
     let args : Vec<String> = env::args().collect();
 
-    let ring = Ring::new(8);
+    let ring = IoUring::new(8).expect("Ring creation failed");
+    // let ring = Ring::new(io_uring).expect("Ring struct creation failed");
 
-    // if args.len() > 2 {
-    //     println!("Please enter a valid address");
-    //     return;
-    // }
-
-    // let ip_port = "127.0.0.1:8080";
     let ip_port = args.get(1).expect("Argument error");
     
-    let sckt = tcp_connect(ip_port).expect("Error when creating TCP socket");
-    // let data = ""; // TODO: Parameterize the data
+    scan(ring, ip_port).expect("Error when creating TCP socket");
 
-    send_tcp_packet(sckt);
+    // TODO: 
+
+    // send_tcp_packet(ip_port);
     
 }
 
