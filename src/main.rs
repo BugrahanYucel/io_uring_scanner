ec741262aea3271052f912f1fa83fea243426a07
BugrahanYucel <Bugrahan57@hotmail.com>
2023-07-31 10:49:52 +0300

    Removed unnecessary code Fixed subnet parsing

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,17 @@ use std::{env, array};
 use nix::sys::socket::{socket, AddressFamily, SockFlag, SockType, SockaddrLike, SockaddrIn, SockProtocol};
 use libc::{self, iovec};
 
-use pnet::packet::{Packet, icmp};
+use std::net::{Ipv4Addr};
+
+use ipnet::{IpSubnets, Ipv4Subnets};
+
+// TODO: Is using static lifetime OK here?
+#[derive(Clone)]
+struct SubnetInfo {
+    ip_start : String,
+    ports : Vec<u16>,
+    subnet_len : u8,
+}
 
 struct Ring {
     entries : u32,
@@ -55,31 +65,52 @@ impl Ring {
 
 } 
 
-fn scan (ring : IoUring, ip_port : &str) -> Result<i32, io::Error>
-{
-    println!("Trying to connect to: {}", ip_port);
+fn scan (
+    ring : IoUring,
+    ip_start : &str,
+    ports : Vec<u16>,
+    subnet_len: u8
+) -> Result<i32, io::Error> {
 
-    // Parse the ip and port
-    let mut ip_port_itr = ip_port.splitn(2, ":");
-    let ip = std::net::IpAddr::from_str(ip_port_itr.next().expect("")).expect("");
-    let port = ip_port_itr.next().expect("").parse::<u16>().unwrap();
-    
-    let ip_str = ip.to_string();
-    let ip_itr : Vec<&str> = ip_str.splitn(4, ".").collect();
+    let max_ip = "255.255.255.255";
 
-    let mut ip_bytes : [u8; 4] = [0; 4];
-    for i in 0..4 {
-        ip_bytes[i] = ip_itr[i].parse::<u8>().unwrap();
+    // TODO: Loop here
+    let subnets: IpSubnets = IpSubnets::from(Ipv4Subnets::new(
+        ip_start.parse().unwrap(),
+        max_ip.parse().unwrap(),
+        subnet_len
+    ));
+
+    for ip in subnets {
+        println!("{}", ip.to_string());
     }
-    println!("Trying: {:?}", ip_bytes);
 
-    let addr = SockaddrIn::new(
-        ip_bytes[0],
-        ip_bytes[1],
-        ip_bytes[2],
-        ip_bytes[3],
-        port
-    );
+    // println!("Trying to connect to: {}", ip_port);
+
+    // TODO: Uncomment
+    // Parse the ip and port
+    // let mut ip_port_itr = ip_port.splitn(2, ":");
+    // let ip = std::net::IpAddr::from_str(ip_port_itr.next().expect("")).expect("");
+    // let port = ip_port_itr.next().expect("").parse::<u16>().unwrap();
+    
+    // let ip_str = ip.to_string();
+    // let ip_itr : Vec<&str> = ip_str.splitn(4, ".").collect();
+
+    // let mut ip_bytes : [u8; 4] = [0; 4];
+    // for i in 0..4 {
+    //     ip_bytes[i] = ip_itr[i].parse::<u8>().unwrap();
+    // }
+
+    // TODO: Uncomment
+    // println!("Trying: {:?}", ip_bytes);
+
+    // let addr = SockaddrIn::new(
+    //     ip_bytes[0],
+    //     ip_bytes[1],
+    //     ip_bytes[2],
+    //     ip_bytes[3],
+    //     port
+    // );
 
     // Create a socket
     let sckt = socket(
@@ -90,25 +121,24 @@ fn scan (ring : IoUring, ip_port : &str) -> Result<i32, io::Error>
     ).expect("TCP socket creation failed");
 
     // Set the socket to non-blocking mode
-    // TODO: Chek if as_raw_fd() mmethod is needed or not
+    // TODO: Chek if as_raw_fd() method is needed or not
     // nix::fcntl::fcntl(sckt, nix::fcntl::FcntlArg::F_SETFL(nix::fcntl::OFlag::O_NONBLOCK)).expect("Failed to set socket to non-blocking");
 
     // TODO: Do this with a range of ips and ports
-    connect(sckt, addr, ring).expect("Error when trying to connect");
+    // connect(sckt, addr, ring).expect("Error when trying to connect");
 
     Ok(sckt)
 
 }
 
 /*
-* Connection
+* TCP connect to a single port
 */
 fn connect (
     sckt : i32, 
     addr : SockaddrIn,
     mut ring : IoUring
     ) -> Result<i32, Error> {
-
 
     // Register opcode to establish connection
     let op_connect: squeue::Entry = opcode::Connect::new(
@@ -131,7 +161,6 @@ fn connect (
     }
     let _ = ring.submit();
 
-    // TODO: Get connection result
     let cqe = ring.completion().next().expect("Completion queue is empty");
 
     if cqe.result() >= 0 {
@@ -144,7 +173,6 @@ fn connect (
 
 }
 
-// TODO: Parameterize data
 fn send_tcp_packet (
     sckt : i32
 ) -> Result<i32, Error> {
@@ -212,18 +240,55 @@ fn read_response (
     };
 }
 
+// Input format is "ip/range" like "192.168.1.0/24"
+fn parse_subnet (subnet_str : &str, ports_str : String) -> SubnetInfo
+{
+    let mut ip_itr = subnet_str.splitn(2, "/");
+    let ip_start = ip_itr.next().unwrap();
+    let subnet_len = ip_itr.next().unwrap().parse::<u8>().unwrap();
+    let ports = ports_str.split(",")
+    .map(|s| s.to_string()
+    .parse::<u16>().expect("Parse error"))
+    .collect();
+
+    // Subnet specifier must be between 0 and 32
+    if subnet_len > 32 {
+        panic!("Subnet length {} is out of bounds [0,32]", subnet_len);
+    }
+
+    let subnet_info: SubnetInfo = SubnetInfo {
+        ip_start: ip_start.to_string(),
+        ports: ports,
+        subnet_len: subnet_len,
+    };
+
+    return subnet_info
+}
+
 fn main() {
     // Take arguments
     let args : Vec<String> = env::args().collect();
 
-    let ring = IoUring::new(8).expect("Ring creation failed");
     // let ring = Ring::new(io_uring).expect("Ring struct creation failed");
-
-    let ip_port = args.get(1).expect("Argument error");
     
-    scan(ring, ip_port).expect("Error when creating TCP socket");
+    let subnet_str = args.get(1).expect("Argument error: Wrong ip range format");
+    let ports_str = args.get(2).expect("Argument error: Wrong ports format");
+    
+    let subnet_info = parse_subnet(subnet_str, ports_str.to_string());
+    
+    println!("ip_start: {}\nports: {:?}\nsubnet_len: {}", 
+    subnet_info.ip_start,
+    subnet_info.ports,
+    subnet_info.subnet_len
+    );
 
-    // TODO: 
+    let ring = IoUring::new(8).expect("Ring creation failed");
+    scan(
+        ring,
+        &subnet_info.ip_start,
+        subnet_info.ports,
+        subnet_info.subnet_len,
+    ).expect("Error when creating TCP socket");
 
     // send_tcp_packet(ip_port);
     
